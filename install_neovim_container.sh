89afc8656a23a518c2755474faa606d1448ed646
Dennis Bakhuis <git@bakhuis.nu>
2024-03-13 16:39:24 +0100

    updated install script

--- install_neovim_container.sh
+++ install_neovim_container.sh
@@ -1,33 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 # Neovim Container - A devcontainer-like experience for Neovim.
 # MIT License - Copyright (c) 2024 Dennis Bakhuis
+
 set +e
 
 NEOVIM_CONTAINER_REPO="https://github.com/dennisbakhuis/neovim_container.git"
-NEOVIM_CONTAINER_TEMP_FOLDER="neovim_container_temp"
 NEOVIM_CONTAINER_TARGET_FOLDER=".neovim"
 
-# Check if git is installed
-if ! [ -x "$(command -v git)" ]; then
-	echo "Error: git is not installed." >&2
+FILES_TO_DOWNLOAD=(
+	"neovim_container/constants.sh"
+	"neovim_container/container.sh"
+	"neovim_container/build.sh"
+	"neovim_container/install.sh"
+	"neovim_container/run.sh"
+	"README.md"
+	"LICENSE"
+)
+
+if ! [ -x "$(command -v wget)" ]; then
+	echo "Error: wget is not installed." >&2
 	exit 1
 fi
 
-# Check if current folder already contains a .neovim folder
 if [ -d "$NEOVIM_CONTAINER_TARGET_FOLDER" ]; then
 	echo "Error: $NEOVIM_CONTAINER_TARGET_FOLDER folder already exists." >&2
 	exit 1
 fi
 
-# Do a partial clone (experimental feature)
-# git clone --no-checkout --depth=1 $NEOVIM_CONTAINER_REPO $NEOVIM_CONTAINER_TEMP_FOLDER
-# cd $NEOVIM_CONTAINER_TEMP_FOLDER
-# git checkout main -- neovim_container README.md LICENSE
-# cd ..
+mkdir -p "$NEOVIM_CONTAINER_TARGET_FOLDER/neovim_container"
+RAW_CONTENT_URL=$(echo $NEOVIM_CONTAINER_REPO | sed 's/github\.com/raw.githubusercontent.com/' | sed 's/\.git$/\/main/')
 
-# Full clone
-git clone $NEOVIM_CONTAINER_REPO $NEOVIM_CONTAINER_TEMP_FOLDER
-mv $NEOVIM_CONTAINER_TEMP_FOLDER/neovim_container $NEOVIM_CONTAINER_TARGET_FOLDER
-mv $NEOVIM_CONTAINER_TEMP_FOLDER/README.md $NEOVIM_CONTAINER_TARGET_FOLDER/
-mv $NEOVIM_CONTAINER_TEMP_FOLDER/LICENSE $NEOVIM_CONTAINER_TARGET_FOLDER/
-rm -rf $NEOVIM_CONTAINER_TEMP_FOLDER
+for file in "${FILES_TO_DOWNLOAD[@]}"; do
+	wget "${RAW_CONTENT_URL}/${file}" --output-document "${NEOVIM_CONTAINER_TARGET_FOLDER}/${file}" --quiet &
+done
+wait
+
+mv $NEOVIM_CONTAINER_TARGET_FOLDER/neovim_container/* $NEOVIM_CONTAINER_TARGET_FOLDER
+rm -rf $NEOVIM_CONTAINER_TARGET_FOLDER/neovim_container
+
+echo "Neovim Container installed in $NEOVIM_CONTAINER_TARGET_FOLDER"
+echo "To start the container, run $NEOVIM_CONTAINER_TARGET_FOLDER/container.sh up"
